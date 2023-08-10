044bd0ab89ef8d51a74d0d5aab4ee4a9f664e29c
Vylee <87347391+Vylee29@users.noreply.github.com>
2023-08-10 11:23:21 +0700

    update interface & interact with trip card

--- src/pages/s2Booking/index.js
+++ src/pages/s2Booking/index.js
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
 import "./s2Booking.scss";
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 // import { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete';
 import ItemTrip from "./itemTrip";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
-import PlacesAutocomplete from 'react-places-autocomplete';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import PlacesAutocomplete from "react-places-autocomplete";
 import {
   geocodeByAddress,
   geocodeByPlaceId,
   getLatLng,
-} from 'react-places-autocomplete';
+} from "react-places-autocomplete";
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const S2Booking = () => {
   const [coords, setCoords] = useState({
     lat: 10.99835602,
-    lng: 77.01502627
-  })
-  const [address, setAddress] = useState('')
-  const handleChange = address => {
+    lng: 77.01502627,
+  });
+  const [address, setAddress] = useState("");
+  const [tripInfo, setTripInfo] = useState(() => {
+    return { telephone: "", address: "", driveType: "" };
+  });
+  const handleChangeTripInformation = (tripInfo) => {
+    console.log("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+    setTripInfo(tripInfo);
+    console.log(tripInfo);
+  };
+  const handleChange = (address) => {
     setAddress(address);
   };
 
-  const handleSelect = address => {
+  const handleSelect = (address) => {
     geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error));
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => console.log("Success", latLng))
+      .catch((error) => console.error("Error", error));
   };
   // const defaultProps = {
   //   center: {
@@ -38,23 +46,12 @@ const S2Booking = () => {
   // };
   const onChangeAddress = async (e) => {
     console.log(e.target.value);
-    const result = await geocodeByAddress("quận")
+    const result = await geocodeByAddress("quận");
     console.log(result);
     // const lnglat = await getLatLng(result[0])
     // setCoords(lnglat);
-  }
-  const data1 = [
-    "Australia",
-    "Austria",
-    "Brazil",
-    "Canada",
-    "Denmark",
-    "Egypt",
-    "France",
-    "Germany",
-    "USA",
-    "Vietnam",
-  ];
+  };
+
   return (
     <React.Fragment>
       <div className="nd-book-driver-page-callcenter-k45" id="877:5254">
@@ -86,9 +83,12 @@ const S2Booking = () => {
                 />
               </div>
             </div>
-            <ItemTrip />
-            <ItemTrip />
-            <ItemTrip />
+            <ItemTrip
+              tripInfo={tripInfo}
+              onClick={handleChangeTripInformation}
+            />
+            {/* <ItemTrip tripInfo={tripInfo} />
+            <ItemTrip tripInfo={tripInfo} /> */}
           </div>
           <div className="auto-group-bk2q-Bt1" id="BCK2SUtwZpWzWsTXgVBk2q">
             <div className="section-header-82Z" id="877:5256">
@@ -105,7 +105,7 @@ const S2Booking = () => {
                   Số điện thoại
                 </p>
                 <p className="website-jCh" id="877:5263">
-                  Website
+                  {tripInfo.telephone}
                 </p>
               </div>
             </div>
@@ -119,7 +119,7 @@ const S2Booking = () => {
                   className="available-products-at-our-website-pQV"
                   id="877:5271"
                 >
-                  Available products at our website
+                  {tripInfo.driveType}
                 </p>
               </div>
             </div>
@@ -129,7 +129,12 @@ const S2Booking = () => {
               onChange={handleChange}
               onSelect={handleSelect}
             >
-              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+              {({
+                getInputProps,
+                suggestions,
+                getSuggestionItemProps,
+                loading,
+              }) => (
                 <div>
                   {/* <input
                   className="available-products-at-our-website-znq"
@@ -143,21 +148,23 @@ const S2Booking = () => {
                       </p>
                       <input
                         {...getInputProps({
-                          placeholder: 'Nhập địa chỉ đã được định vị',
-                          className: 'available-products-at-our-website-znq',
+                          placeholder: "Nhập địa chỉ đã được định vị",
+                          className: "available-products-at-our-website-znq",
                         })}
-                      /></div>
+                        value={tripInfo.address}
+                      />
+                    </div>
                   </div>
-                  <div className="autocomplete-dropdown-container " >
+                  <div className="autocomplete-dropdown-container ">
                     {loading && <div>Loading...</div>}
-                    {suggestions.map(suggestion => {
+                    {suggestions.map((suggestion) => {
                       const className = suggestion.active
-                        ? 'suggestion-item--active'
-                        : 'suggestion-item';
+                        ? "suggestion-item--active"
+                        : "suggestion-item";
                       // inline style for demonstration purpose
                       const style = suggestion.active
-                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                        : { backgroundColor: '#f9fafb', cursor: 'pointer' };
+                        ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                        : { backgroundColor: "#f9fafb", cursor: "pointer" };
                       return (
                         <div
                           {...getSuggestionItemProps(suggestion, {
@@ -166,11 +173,28 @@ const S2Booking = () => {
                           })}
                         >
                           {/* <span className="textSuggestion">11 </span> */}
-                          <svg className="iconSvg" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
-                            <circle cx="12.5" cy="12.5" r="12.5" fill="#FA8D1D" />
-                            <path d="M12.176 18.4256C11.2989 17.7606 10.4859 17.0317 9.74549 16.2464C8.63438 15.067 7.31494 13.3105 7.31494 11.6368C7.31446 10.782 7.59932 9.94626 8.13348 9.2354C8.66763 8.52454 9.42707 7.97048 10.3157 7.64334C11.2043 7.31621 12.1821 7.2307 13.1254 7.39765C14.0687 7.56459 14.9351 7.97648 15.6149 8.58119C16.0675 8.98164 16.4263 9.45789 16.6704 9.98238C16.9145 10.5069 17.0392 11.0692 17.0371 11.6368C17.0371 13.3105 15.7177 15.067 14.6066 16.2464C13.8662 17.0317 13.0532 17.7606 12.176 18.4256ZM12.176 9.78527C11.6235 9.78527 11.0936 9.98034 10.7029 10.3276C10.3122 10.6748 10.0927 11.1457 10.0927 11.6368C10.0927 12.1278 10.3122 12.5987 10.7029 12.946C11.0936 13.2932 11.6235 13.4883 12.176 13.4883C12.7286 13.4883 13.2585 13.2932 13.6492 12.946C14.0399 12.5987 14.2594 12.1278 14.2594 11.6368C14.2594 11.1457 14.0399 10.6748 13.6492 10.3276C13.2585 9.98034 12.7286 9.78527 12.176 9.78527Z" fill="white" />
+                          <svg
+                            className="iconSvg"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="25"
+                            viewBox="0 0 25 25"
+                            fill="none"
+                          >
+                            <circle
+                              cx="12.5"
+                              cy="12.5"
+                              r="12.5"
+                              fill="#FA8D1D"
+                            />
+                            <path
+                              d="M12.176 18.4256C11.2989 17.7606 10.4859 17.0317 9.74549 16.2464C8.63438 15.067 7.31494 13.3105 7.31494 11.6368C7.31446 10.782 7.59932 9.94626 8.13348 9.2354C8.66763 8.52454 9.42707 7.97048 10.3157 7.64334C11.2043 7.31621 12.1821 7.2307 13.1254 7.39765C14.0687 7.56459 14.9351 7.97648 15.6149 8.58119C16.0675 8.98164 16.4263 9.45789 16.6704 9.98238C16.9145 10.5069 17.0392 11.0692 17.0371 11.6368C17.0371 13.3105 15.7177 15.067 14.6066 16.2464C13.8662 17.0317 13.0532 17.7606 12.176 18.4256ZM12.176 9.78527C11.6235 9.78527 11.0936 9.98034 10.7029 10.3276C10.3122 10.6748 10.0927 11.1457 10.0927 11.6368C10.0927 12.1278 10.3122 12.5987 10.7029 12.946C11.0936 13.2932 11.6235 13.4883 12.176 13.4883C12.7286 13.4883 13.2585 13.2932 13.6492 12.946C14.0399 12.5987 14.2594 12.1278 14.2594 11.6368C14.2594 11.1457 14.0399 10.6748 13.6492 10.3276C13.2585 9.98034 12.7286 9.78527 12.176 9.78527Z"
+                              fill="white"
+                            />
                           </svg>
-                          <span className="textSuggestion">: {suggestion.description}</span>
+                          <span className="textSuggestion">
+                            {suggestion.description}
+                          </span>
                         </div>
                       );
                     })}
@@ -179,13 +203,13 @@ const S2Booking = () => {
               )}
             </PlacesAutocomplete>
 
-
             <div className="rectangle-1748-hjB" id="877:5273"></div>
-
 
             <div className="maps-zoom-levels-3-street-HSV">
               <GoogleMapReact
-                bootstrapURLKeys={{ key: "AIzaSyBLAnygT3LzvYGdMD43t12_zw79CXC0O2w" }}
+                bootstrapURLKeys={{
+                  key: "AIzaSyBLAnygT3LzvYGdMD43t12_zw79CXC0O2w",
+                }}
                 center={coords}
                 defaultZoom={18}
               >
@@ -193,7 +217,10 @@ const S2Booking = () => {
                   lat={coords.lat}
                   lng={coords.lng}
                   text={
-                    <FontAwesomeIcon icon={faMapMarkerAlt} style={{ color: "#ff0000", fontSize: "20" }} />
+                    <FontAwesomeIcon
+                      icon={faMapMarkerAlt}
+                      style={{ color: "#ff0000", fontSize: "20" }}
+                    />
                   }
                 />
               </GoogleMapReact>
@@ -203,9 +230,7 @@ const S2Booking = () => {
             </div>
           </div>
         </div>
-        <div className="googleMapCSS">
-
-        </div>
+        <div className="googleMapCSS"></div>
       </div>
     </React.Fragment>
   );
