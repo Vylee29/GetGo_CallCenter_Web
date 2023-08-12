fae212ccc75c4d3f4ff6e119b9b3a8496498d2eb
doanhuuloc <doanhuuloc101102@gmail.com>
2023-08-12 21:27:05 +0700

    s1book book success:

--- src/pages/s1Booking/index.js
+++ src/pages/s1Booking/index.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import "./s1Booking.scss";
 import axios from "axios";
-
-
-
-function HistoryItem({ data }) {
-  let trip =JSON.parse(data.start)
-  console.log(trip);
+import { useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+const Notification = ({ message, onClose }) => {
   return (
-    <div className="configuration-card-Kpy">
-      <div className="content-qHX">
+    <div className="notification">
+      <FontAwesomeIcon className="facircle" icon={faCheckCircle} />
+      <span className="message">{message}</span>
+      <button className="close-button" onClick={onClose}>
+        Đóng
+      </button>
+    </div>
+  );
+};
+
+function HistoryItem({ data,  handleBookCar }) {
+  let trip = JSON.parse(data.start)
+  // console.log(data);
+  return (
+    <div className="configuration-card-Kpy" onClick={()=>handleBookCar(trip)}>
+      <div className="content-qHX" >
         <div className="svgFrame">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -27,7 +40,7 @@ function HistoryItem({ data }) {
         </div>
         <div>
           <p className="a-ch-th-nht-yed">{trip.place}</p>
-          <div className="frame-78-auK">
+          {/* <div className="frame-78-auK">
             <p
               className="hooded-bathrobe-classic-bathrobe-Wny"
               id="877:5227"
@@ -35,7 +48,7 @@ function HistoryItem({ data }) {
               Thành phố Hồ Chí Minh
             </p>
             <p className="more-dsb" id="877:5228"></p>
-          </div>
+          </div> */}
         </div>
 
       </div>
@@ -44,10 +57,30 @@ function HistoryItem({ data }) {
 }
 const S1Booking = () => {
   const [phone, setPhone] = useState("");
+  const [errMsgPhone, setErrMsgPhone] = useState('');
   const [address, setAddress] = useState("");
+  const [errMsgAdd, setErrMsgAdd] = useState('');
+  const [listCar, setListCar] = useState([
+    { "carType": 1, "name": "Honda 4 chỗ" },
+    { "carType": 2, "name": "Honda 7 chỗ" }
+  ]);
   const [car, setCar] = useState("");
+  const [errMsgCar, setErrMsgCar] = useState('');
   const [history, setHistory] = useState([]);
 
+
+  const [showNotification, setShowNotification] = useState(false);
+  const [message, setMessage] = useState("");
+  const handleNotificationClose = () => {
+    setShowNotification(false);
+  };
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:3001/getallvehicles").then(function (response) {
+  //     setListCar(response.data)
+
+  //   }).catch(function (error) { console.log(error); })
+  // }, [])
 
   const phoneRegex = /^[0-9]{10}$/;
 
@@ -55,18 +88,18 @@ const S1Booking = () => {
     setPhone(e.target.value);
 
     if (phoneRegex.test(e.target.value)) {
+      console.log(phoneRegex.test(e.target.value));
       let convertPhone = '+84' + e.target.value.slice(1)
       axios
         .get("http://localhost:3001/v1/history/phone?phone=" + convertPhone)
         .then(function (response) {
-          console.log(response.data.trips);
+          console.log(response.data);
           setHistory(response.data.trips)
         })
         .catch(function (error) {
-          console.log(error);
         })
-
-
+    } else {
+      setHistory([])
     }
   };
   const handleAddressInput = (e) => {
@@ -76,8 +109,61 @@ const S1Booking = () => {
     setCar(e.target.value);
   };
 
+  const handleSubmit = () => {
+    let flag = true
+    if (!phoneRegex.test(phone)) {
+      setErrMsgPhone("Số điện thoại không đúng định dạng")
+      flag = false
+    } else {
+      setErrMsgPhone("")
+    }
+
+    if (!address) {
+      setErrMsgAdd("Địa chỉ không được để trống")
+      flag = false
+    } else {
+      setErrMsgAdd("")
+    }
+
+    if (!car) {
+      setErrMsgCar("Loại xe không được để trống")
+      flag = false
+    } else {
+      setErrMsgCar("")
+    }
+
+    if (flag) {
+      const data = {
+        "start": { "place": address },
+        "phone": '+84' + phone.slice(1),
+        "carType": car,
+      }
+      console.log(data);
+      axios
+        .post("http://localhost:3001/v1/booking/callcenters1", data, {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(function (response) {
+          console.log(response);
+          if (response.data.statusCode == 200)
+            setMessage("Định vị chuyến đi thành công")
+          else setMessage("Định vị chuyến đi thất bại")
+          setShowNotification(true)
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+  }
+
+
   return (
     <React.Fragment>
+      {showNotification && (
+        <Notification message={message} onClose={handleNotificationClose} />
+      )}
       <div className="st-book-driver-page-callcenter-C13">
         <p className="t-xe-cho-khch-QN1">ĐẶT XE CHO KHÁCH</p>
         <div className="section-header-Z1w">
@@ -88,13 +174,14 @@ const S1Booking = () => {
         </div>
         <div className="input-LTw">
           <div className="frame-79-GMb">
-            <p className="s-in-thoi-o6d">Số điện thoại</p>
+            <p className="s-in-thoi-o6d">Số điện thoại </p>
             <input
               className="website-hC1"
               placeholder="Nhập số điện thoại khách hàng"
               value={phone}
               onChange={handlePhoneInput}
             ></input>
+            {errMsgPhone && <p className="errMsgInput">{errMsgPhone}</p>}
           </div>
         </div>
         <div className="input-P4q">
@@ -106,17 +193,26 @@ const S1Booking = () => {
               value={address}
               onChange={handleAddressInput}
             ></input>
+            {errMsgAdd && <p className="errMsgInput">{errMsgAdd}</p>}
+
           </div>
         </div>
         <div className="input-mUh">
           <div className="frame-79-WBP">
             <p className="loi-xe-3SD">Loại xe</p>
-            <input
+            {/* <input
               className="available-products-at-our-website-BHX"
               placeholder="Nhập loại xe khách hàng mong muốn"
               value={car}
               onChange={handleCarInput}
-            ></input>
+            ></input> */}
+            <select className="selectTypeCar" value={car} onChange={handleCarInput} >
+              <option value="">Chọn loại xe khách hàng mong muốn</option>
+              {listCar.map((item, index) => <option key={index} value={item.carType}>{item.name}</option>)}
+
+            </select>
+            {errMsgCar && <p className="errMsgInput">{errMsgCar}</p>}
+
           </div>
         </div>
         <div className="rectangle-1748-MbK"></div>
@@ -127,9 +223,9 @@ const S1Booking = () => {
           Tìm kiếm chuyến đi gần nhất từ SĐT khách hàng
         </p>
         <div className="listHistory">
-          {history.map((item, index) => <HistoryItem key={index} data={item} />)}
+          {history.map((item, index) => <HistoryItem key={index} data={item} handleBookCar={handleBookCar}/>)}
 
-          <button className="input-tBw">ĐỊNH VỊ </button>
+          <button className="input-tBw" onClick={handleSubmit} >ĐỊNH VỊ </button>
         </div>
 
       </div>
